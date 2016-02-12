bd71c72cd371d5b3c16d05dcd95b796ff8fe688b
jayvisariya93 <jayvisariya93@gmail.com>
2016-02-12 16:58:35 +0530

    bgsync

--- app/src/main/java/com/noteshareapp/noteshare/MainActivity.java
+++ app/src/main/java/com/noteshareapp/noteshare/MainActivity.java
@@ -1,7 +1,10 @@
 package com.noteshareapp.noteshare;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -102,6 +105,31 @@ public class MainActivity extends DrawerActivity {
     boolean outsideNote = true;
     Typeface faceBold, faceMedium;
 
+
+
+
+
+    // Constants
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "com.noteshareapp.bgSync.provider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "noteshareapp.com";
+    // The account name
+    public static final String ACCOUNT = "noteshareapp";
+    // Instance fields
+    Account mAccount;
+
+    public static final long SECONDS_PER_MINUTE = 60L;
+    public static final long SYNC_INTERVAL_IN_MINUTES = 2L;
+    public static final long SYNC_INTERVAL =
+            SYNC_INTERVAL_IN_MINUTES *
+                    SECONDS_PER_MINUTE;
+    // Global variables
+    // A content resolver for accessing the provider
+    ContentResolver mResolver;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,6 +145,22 @@ public class MainActivity extends DrawerActivity {
                 .inflate(R.layout.activity_main, null, false);
         mDrawerLayout.addView(contentView, 0);
         //DataManager.sharedDataManager().setSelectedIndex(-1);
+
+
+
+        mAccount = CreateSyncAccount(this);
+        mResolver = getContentResolver();
+        /*
+         * Turn on periodic syncing
+         */
+        ContentResolver.addPeriodicSync(
+                mAccount,
+                AUTHORITY,
+                Bundle.EMPTY,
+                SYNC_INTERVAL);
+
+
+
 
         initlizeUIElement(contentView);
 
@@ -217,8 +261,47 @@ public class MainActivity extends DrawerActivity {
             }
         }, 300);
 
-        getNotificationCount();
+        //getNotificationCount();
     }
+
+
+
+
+
+
+    public static Account CreateSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return newAccount;
+    }
+
+
+
+
+
+
 
     public void getNotificationCount(){
         RegularFunctions.getNotificationCount(this);
